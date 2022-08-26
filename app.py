d27f545c6c4ab12807ab6830078885a77d4331a8
shubhangi893 <71168429+shubhangi893@users.noreply.github.com>
2022-08-26 23:18:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+
+app = Flask(__name__)
+
+
+model = pickle.load(open("model.pkl", "rb"))
+
+
+@app.route("/predict", methods = ["POST"])
+def predict():
+    if request.method == 'POST':
+        json_ = request.json
+        query_df = pd.DataFrame(json_, index=[0])
+        prediction = model.predict(query_df)
+        return jsonify({"Prediction": list(prediction)})
+
+if __name__ == "__main__":
+        app.run(port=5000,debug=True)
